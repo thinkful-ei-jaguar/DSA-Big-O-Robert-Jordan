45697f39e288027b52be0cde73d9e96f627e73d4
Alex <robert.perez6@g.austincc.edu>
2020-03-03 10:42:39 -0600

    #2 #3

--- drills.js
+++ drills.js
@@ -2,3 +2,28 @@
 // 1) Constant O(1) - our input size does not matter and we are looking for one particular answer 
 // 2) Liner O(n) - our output (answer from the individuals) grows linear with our input (the amount of time we ask the questiong)
 
+
+//Drill #2
+/* function isEven(value) {
+    if (value % 2 == 0) {
+        return true;
+    }
+    else
+        return false;
+    }
+} */
+//Constant O(1)- our input is 1 value so it will return 1 value back,runtime does not depend on size of input
+
+//Drill #3
+/* function areYouHere(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        const el1 = arr1[i];
+        for (let j = 0; j < arr2.length; j++) {
+            const el2 = arr2[j];
+            if (el1 === el2) return true;
+        }
+    }
+    return false;
+} */
+//polynomial O(n^2)- since there is a nested loop the function is polynomial taking that much time to solve.
+
