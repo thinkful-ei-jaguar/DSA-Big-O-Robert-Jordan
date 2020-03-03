81afab94f17bf3dab18acc7160c607980a8c1a4a
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-03 08:36:07 -0800

    drill #1 complete

---
+++ drills.js
@@ -0,0 +1,4 @@
+//Drill #1 
+// 1) Constant O(1) - our input size does not matter and we are looking for one particular answer 
+// 2) Liner O(n) - our output (answer from the individuals) grows linear with our input (the amount of time we ask the questiong)
+
